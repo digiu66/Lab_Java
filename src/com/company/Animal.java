739f61403a1f900fc61cbecf646add3be1236b35
digiu <digiu66@gmail.com>
2020-04-21 17:54:57 +0200

    Nakarmiono i wyprowadzono mojego psa na spacer (ja siedze na zajeciach, moja kobita to robi za mnie)

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,45 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Animal {
+    String name;
+    final String species;
+    private Double weight;
+
+    static final Double DEFAULT_DOG_WEIGHT = 5.0; //small dogs are cooler I guess
+    static final Double DEFAULT_CAT_WEIGHT = 3.0;
+
+    public Animal(String species) {
+        this.species = species;
+        if (species == "dog") {
+            this.weight = DEFAULT_DOG_WEIGHT;
+        }
+        else if (species == "cat") {
+            this.weight = DEFAULT_CAT_WEIGHT;
+        }
+    }
+
+
+    void feed() {
+        if (weight > 0) {
+            weight++;
+            System.out.println("I weigh " + weight);
+        }
+
+        else {
+            System.out.println("I'm dead and I can't eat no more.");
+        }
+    }
+
+
+    void takeForAWalk() {
+        weight--;
+        if (weight <= 0) {
+            System.out.println("I'm dead and I can't walk no more");
+        }
+        else {
+            System.out.println("I like walking with you.");
+        }
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Main {
+
+    public static void main(String [] args) {
+
+        Animal dog = new Animal("dog");
+        dog.name = "Luska";
+
+
+        Human myself = new Human();
+        myself.firstName = "Igor";
+        myself.lastName = "Banach";
+
+
+        myself.pet = dog;
+        myself.pet.feed();
+        myself.pet.takeForAWalk();
+    }
 }
