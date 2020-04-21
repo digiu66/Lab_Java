269342bc2bfdb6bed9efdb2f8e1f0ab9b509e039
digiu <digiu66@gmail.com>
2020-04-21 16:25:54 +0200

    Zadeklarowano klase "Car"

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Car {
+    public final String brand;
+    public final String model;
+    public final Integer yearOfProduction;
+    public final Integer enginePower;
+
+
+    public String plates;
+    public Integer value = 330;
+
+
+    public Car(String brand, String model, Integer yearofProduction, Integer enginePower) {
+        this.brand = brand;
+        this.model = model;
+        this.yearOfProduction = yearofProduction;
+        this.enginePower = enginePower;
+    }
 }
